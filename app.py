5a20e5c44bb8d94c91e0378a743e5a0df87b9558
Shehryar Ahmed Khan <shehryarahmedkhan39@gmail.com>
2025-08-08 01:14:53 +0500

    Final commit before Render deployment

--- app.py
+++ app.py
@@ -167,7 +167,4 @@ def send_suggestions_and_feedback(phone):
     user_states[phone]["suggested"] = True
 
 if __name__ == '__main__':
-    from waitress import serve
-    port = int(os.environ.get("PORT", 5000))
-    serve(app, host='0.0.0.0', port=port)
-
+    app.run()
